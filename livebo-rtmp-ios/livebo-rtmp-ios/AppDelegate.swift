3b702b5a9062899e83b9f9b1b1f04b2d26b3484e
yqb <yqb@163.com>
2021-01-14 07:26:52 +0800

    f

--- livebo-rtmp-ios/livebo-rtmp-ios/AppDelegate.swift
+++ livebo-rtmp-ios/livebo-rtmp-ios/AppDelegate.swift
@@ -1,7 +1,7 @@
 //
 //  AppDelegate.swift
 //  SwiftUIDemo
-//  rtmp  是
+//  rtmp  是发
 //  Created by Lee on 2019/9/24.
 //  Copyright © 2019 Lee. All rights reserved.
 ///Users/yqb/Desktop/swfitcode/SwiftUIDemo-master/SwiftUIDemo/BlurView.swift
